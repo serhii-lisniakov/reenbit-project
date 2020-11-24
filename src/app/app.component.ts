169d8da741b5ba747bed68d62dc2cd5479931239
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-11-24 14:29:05 +0200

    project initialization

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+interface Hero {
+  id: number;
+  name: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Freshnesecom';
+}
