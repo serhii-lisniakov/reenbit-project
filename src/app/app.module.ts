85b77486eb1d99ca677289c0742b62e0bc72fb7a
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-11-24 16:14:43 +0200

    header component with inner ones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderTopComponent } from './header/header-top/header-top.component';
+import { NavComponent } from './header/nav/nav.component';
+import { BreadCrumbsComponent } from './header/bread-crumbs/bread-crumbs.component';
+import { SearchComponent } from './header/search/search.component';
+import { HeaderProfileComponent } from './header/header-profile/header-profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HeaderTopComponent,
+    NavComponent,
+    BreadCrumbsComponent,
+    SearchComponent,
+    HeaderProfileComponent
   ],
   imports: [
     BrowserModule,
