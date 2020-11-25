9fd7595cadbd6a6a5c1b5fae558817b155a05a18
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-11-25 14:18:42 +0200

    footer skeleton

---
+++ src/app/footer/footer-tags/footer-tags.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+const mockTags = ['Beans', 'Carrots', 'Apples'];
+
+@Component({
+  selector: 'app-footer-tags',
+  templateUrl: './footer-tags.component.html',
+  styleUrls: ['./footer-tags.component.scss']
+})
+export class FooterTagsComponent implements OnInit {
+  tags: string[] = mockTags;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
