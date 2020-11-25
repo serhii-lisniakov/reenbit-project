0d648d14bc7868baf9719a992472b677bd91021e
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-11-25 14:19:00 +0200

    skeleton

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AllProductsComponent} from './body/all-products/all-products.component';
+import {ProductComponent} from './body/product/product.component';
+import {BasketComponent} from './body/basket/basket.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: AllProductsComponent },
+  { path: 'products/:id', component: ProductComponent },
+  { path: 'basket', component: BasketComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,14 @@ import { NavComponent } from './header/nav/nav.component';
 import { BreadCrumbsComponent } from './header/bread-crumbs/bread-crumbs.component';
 import { SearchComponent } from './header/search/search.component';
 import { HeaderProfileComponent } from './header/header-profile/header-profile.component';
+import { BodyComponent } from './body/body.component';
+import { AllProductsComponent } from './body/all-products/all-products.component';
+import { ProductComponent } from './body/product/product.component';
+import { FiltersComponent } from './body/filters/filters.component';
+import { FooterComponent } from './footer/footer.component';
+import { FooterNavComponent } from './footer/footer-nav/footer-nav.component';
+import { FooterTagsComponent } from './footer/footer-tags/footer-tags.component';
+import { BasketComponent } from './body/basket/basket.component';
 
 @NgModule({
   declarations: [
@@ -18,11 +27,20 @@ import { HeaderProfileComponent } from './header/header-profile/header-profile.c
     NavComponent,
     BreadCrumbsComponent,
     SearchComponent,
-    HeaderProfileComponent
+    HeaderProfileComponent,
+    BodyComponent,
+    AllProductsComponent,
+    ProductComponent,
+    FiltersComponent,
+    FooterComponent,
+    FooterNavComponent,
+    FooterTagsComponent,
+    BasketComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
