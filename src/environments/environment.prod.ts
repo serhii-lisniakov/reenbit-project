987a99afd60dea7d59b84f01ac04ccda8a5a3c3e
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-11-30 14:09:04 +0200

    changed css backgrounds to img tegs + deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBhHfB1u_FH5aPofp1NbWT3-WR1R9ZniD4',
+    authDomain: 'freshnesecom.firebaseapp.com',
+    databaseURL: 'https://freshnesecom.firebaseio.com',
+    projectId: 'freshnesecom',
+    storageBucket: 'freshnesecom.appspot.com',
+    messagingSenderId: '83106748447',
+    appId: '1:83106748447:web:407b9cf51585b0a690dd2a'
+  }
 };
