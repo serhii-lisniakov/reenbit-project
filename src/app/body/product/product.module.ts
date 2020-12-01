18b0e267a8b6268db718bf37ae79353fe86bfe84
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-12-01 11:57:21 +0200

    added tabs on the product-details page

--- src/app/body/product/product.module.ts
+++ src/app/body/product/product.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ProductRoutingModule } from './product-routing.module';
 import { ProductComponent } from './product.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { TabComponent } from './tab/tab.component';
 
 
 @NgModule({
   declarations: [
-    ProductComponent
+    ProductComponent,
+    TabsComponent,
+    TabComponent
   ],
   imports: [
     CommonModule,
