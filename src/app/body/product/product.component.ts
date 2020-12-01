2e634ec6f4bd80a177bafe25314bc7f183c9b310
serhiilisniakov <serhiy.lisnyakov@gmail.com>
2020-12-01 15:17:35 +0200

    fixed img styles on product-detail page; fixed proposals links

--- src/app/body/product/product.component.ts
+++ src/app/body/product/product.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
-import {ProductService} from '../../services/product.service';
+import { ProductService } from '../../services/product.service';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
+  private destroy$ = new Subject<undefined>();
   public product: Product;
   public productRating: string[];
   public proposals: Product[];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService,
+              private route: ActivatedRoute) {
+    this.route.params.pipe().subscribe((params: Params) => {
+      this.product = this.productService.product;
+    });
+  }
 
   ngOnInit(): void {
     this.product = this.productService.product;
@@ -34,6 +41,11 @@ export class ProductComponent implements OnInit {
       randomProposals.push(this.productService.products[getRandomNumber(0, this.productService.products.length)]);
     }
     return randomProposals;
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
 }
